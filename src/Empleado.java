bcc808e86d2517f9e4a30d6a5c228f69a9ef7c06
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2024-11-28 23:52:35 -0500

    Empleado.java

--- src/Empleado.java
+++ src/Empleado.java
@@ -9,19 +9,4 @@ public abstract class Empleado extends Persona implements Contrato{
         this.tipoDeTrabajo = tipoDeTrabajo;
         this.zonaDeTrabajo = zonaDeTrabajo;
     }
-
-    @Override
-    public float getCantidadHoras() {
-        return 0;
-    }
-
-    @Override
-    public float getCantidadPago() {
-        return 0;
-    }
-
-    @Override
-    public boolean trabajar() {
-        return false;
-    }
 }
